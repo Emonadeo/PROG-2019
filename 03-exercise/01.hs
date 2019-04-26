fd16a891ef248cf0bb6ae16377a687ea05ec7b59
emonadeo <emanuel.pilz@live.de>
2019-04-26 14:28:44 +0200

    exercise 03: tasks 01 & 02

--- 03-exercise/01.hs
+++ 03-exercise/01.hs
@@ -1,11 +1,31 @@
 -- Übung 03 - Programmierung TU Dresden
--- Tutor: Claas de Boer
--- Github: https://github.com/cdboer/programmierung-ss19
+-- Tutor: Eric Kunze
+-- Github: https://github.com/oakoneric/programmierung-ss19
 
-
--- Aufgabe 01A
 data Tree = Node Int Tree Tree | NIL
 
-insert :: Tree -> [Int] -> Tree
+tree1 :: Tree
+tree1 = Node 5 (Node 3 NIL NIL) (Node 6 NIL NIL)
 
--- Aufgabe 01B
+-- Aufgabe 01A
+insert :: Tree -> [Int] -> Tree
+insert t [] = t
+insert NIL (x:xs) = insert (Node x NIL NIL) xs
+insert t@(Node n t1 t2) (x:xs)
+    | x < n = insert (Node n (insert t1 [x]) t2) xs
+    | x > n = insert (Node n t1 (insert t2 [x])) xs
+    | otherwise = insert t xs
+
+-- Aufgabe 01B: Klassische Notation
+treeEqual :: Tree -> Tree -> Bool
+treeEqual NIL NIL = True
+treeEqual NIL (Node x t1 t2) = False
+treeEqual (Node x t1 t2) NIL = False
+treeEqual (Node x t1 t2) (Node y ta tb) = (x == y) && (treeEqual t1 ta) && (treeEqual t2 tb)
+
+-- Aufgabe 01B: Operator-/Infixnotation
+treeEq :: Tree -> Tree -> Bool
+treeEq NIL NIL = True
+treeEq NIL (Node x t1 t2) = False
+treeEq (Node x t1 t2) NIL = False
+treeEq (Node x t1 t2) (Node y ta tb) = (x == y) && (t1 `treeEq` ta) && (t2 `treeEq` tb)
--- 03-exercise/02.hs
+++ 03-exercise/02.hs
@@ -1,13 +1,43 @@
 -- Übung 03 - Programmierung TU Dresden
--- Tutor: Claas de Boer
--- Github: https://github.com/cdboer/programmierung-ss19
+-- Tutor: Eric Kunze
+-- Github: https://github.com/oakoneric/programmierung-ss19
 
 import Prelude hiding (even)
 
--- Aufgabe 02A
 data Tree = Node Int [Tree]
 
+-- Aufgabe 02A
 nLeaves :: Tree -> Int
+nLeaves (Node _ []) = 1
+nLeaves (Node _ xxs) = f xxs
+    where
+        f [] = 0
+        f (x:xs) = (nLeaves x) + f xs
+
+-- Aufgabe 02A: Einzelne Funktion
+nLeaves' :: Tree -> Int
+nLeaves' (Node _ []) = 1
+nLeaves' (Node _ [x]) = nLeaves' x
+nLeaves' (Node a (x:xs)) = nLeaves' (Node a [x]) + nLeaves' (Node a xs)
+
+-- Aufgabe 02A: Funktion höherer Ordnung
+nLeaves'' :: Tree -> Int
+nLeaves'' (Node _ []) = 1
+nLeaves'' (Node _ ts) = foldr (+) 0 (map nLeaves'' ts)
 
 -- Aufgabe 02B
 even :: Tree -> Bool
+even (Node a ts) = (length ts) `mod` 2 == 0 && evens ts
+    where
+        evens [] = True
+        evens (x:xs) = even x && evens xs
+
+-- Aufgabe 02B: Einzelne Funktion
+even' :: Tree -> Bool
+even' (Node _ []) = True
+even' (Node _ [x]) = False
+even' (Node a (x:y:xs)) = even' x && even' y && even' (Node a xs)
+
+-- Aufgabe 02B: Funktion höherer Ordnung
+even'' :: Tree -> Bool
+even'' (Node a ts) = (length ts) `mod` 2 == 0 && foldr (&&) True (map even'' ts)
